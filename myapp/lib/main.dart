144ded854725a97d8e41a953b78d22fd800c487c
manvendra5345 <singhmanvendra2003@gmail.com>
2025-02-24 17:58:56 +0530

    Update main.dart

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,28 +1,5 @@
 import 'package:flutter/material.dart';
-import 'pages/login_page.dart';
-import 'pages/landing_page.dart';
-import 'pages/language_select.dart';
-import 'pages/acc_creation.dart';
-import 'pages/Forgot_password.dart';
-import 'pages/cold_storage.dart';
-import 'pages/cold_storage_specific.dart';
-import 'pages/inventory.dart';
-import 'pages/inventory_specific.dart';
-import 'pages/inventory_specific_2.dart';
-import 'pages/item_details.dart';
-import 'pages/helbot.dart';
-import 'pages/settings.dart';
-import 'pages/notification.dart';
-import 'pages/welcome.dart';
-import 'pages/community_forum.dart';
-import 'pages/summary_page.dart';
-import 'pages/summary_page_2.dart';
-import 'pages/summary_page_3.dart';
-import 'pages/community_forum_2.dart';
-import 'pages/new_item.dart';
-import 'pages/mycart.dart';
-import 'pages/order_details.dart';
-import 'pages/Aggregation_service.dart';
+import 'pages/pages.dart';  // Single import for all pages
 
 void main() {
   runApp(const MyApp());
@@ -39,85 +16,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LoginPage(), // Start with LoginPage as the home
+      home: const LoginPage(), // Make sure LoginPage is defined properly
     );
   }
 }
 
-// Example of how each page might look with navigation to the next page
-
-class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) => const LandingPage()));
-          },
-          child: const Text('Go to Landing'),
-        ),
-      ),
-    );
-  }
-}
-
-class LandingPage extends StatelessWidget {
-  const LandingPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) => const LanguageSelect()));
-          },
-          child: const Text('Select Language'),
-        ),
-      ),
-    );
-  }
-}
-
-// Continue this pattern for each page, here are just a few examples:
-
-class LanguageSelect extends StatelessWidget {
-  const LanguageSelect({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) => const AccountCreation()));
-          },
-          child: const Text('Create Account'),
-        ),
-      ),
-    );
-  }
-}
-
-class AccountCreation extends StatelessWidget {
-  const AccountCreation({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) => const ForgotPassword()));
-          },
-          child: const Text('Forgot Password?'),
-        ),
-      ),
-    );
-  }
-}
-
-// ... and so on for each page, where each page has a button or interaction that leads to the next in your sequence
