f69ea7cd66a62a6d317c78438601015070549aad
manvendra5345 <singhmanvendra2003@gmail.com>
2025-03-02 00:42:19 +0530

    Update main.dart

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,6 +1,29 @@
 import 'package:flutter/material.dart';
-import 'pages/pages.dart';  // Single import for all pages
-
+import 'pages/login_page.dart';
+import 'package:myapp/images/images.dart';
+import 'pages/landing_page.dart';
+import 'pages/language_select.dart';
+import 'pages/acc_creation.dart';
+import 'pages/Forgot_password.dart';
+import 'pages/cold_storage.dart';
+import 'pages/cold_storage_specific.dart';
+import 'pages/inventory.dart';
+import 'pages/inventory_specific.dart';
+import 'pages/inventory_specific_2.dart';
+import 'pages/item_details.dart';
+import 'pages/helbot.dart';
+import 'pages/settings.dart';
+import 'pages/notification.dart';
+import 'pages/welcome.dart';
+import 'pages/community_forum.dart';
+import 'pages/summary_page.dart';
+import 'pages/summary_page_2.dart';
+import 'pages/summary_page_3.dart';
+import 'pages/community_forum_2.dart';
+import 'pages/new_item.dart';
+import 'pages/mycart.dart';
+import 'pages/order_details.dart';
+import 'pages/Aggregation_service.dart';
 void main() {
   runApp(const MyApp());
 }
